869c2f90ee989059c6fdefa6098db1ca9c269e02
João Keuwe <91097315+JoaoKeuwe@users.noreply.github.com>
2022-01-19 19:08:14 -0300

    Requisito 03

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as BrowseRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Search from './components/Search';
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowseRouter>
         <Switch>
           <Route exact path="/" component={ Search } />
+          <Route exact path="/ShoppingCart" component={ ShoppingCart } />
         </Switch>
       </BrowseRouter>
     </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Redirect } from 'react-router';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      redirect: false,
+    };
+    this.renderCart = this.renderCart.bind(this);
+  }
+
+  renderCart = () => {
+    this.setState({
+      redirect: true,
+    });
+  }
+
   render() {
+    const { redirect } = this.state;
     return (
       <section>
         <form>
@@ -16,7 +32,17 @@ class Search extends React.Component {
               placeholder="Digite algum termo de pesquisa ou escolha uma categoria."
             />
           </label>
+          <button
+            type="submit"
+            img="search"
+            data-testid="shopping-cart-button"
+            onClick={ this.renderCart }
+          >
+            carrinho
+
+          </button>
         </form>
+        {redirect && <Redirect to="/ShoppingCart" /> }
       </section>
     );
   }
