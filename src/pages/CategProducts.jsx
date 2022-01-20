4bf51b85fda0d74f80921723e516d63d28fe0f0a
João Keuwe <91097315+JoaoKeuwe@users.noreply.github.com>
2022-01-20 18:09:23 -0300

    requisito 07

--- src/pages/CategProducts.jsx
+++ src/pages/CategProducts.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class CategProducts extends React.Component {
@@ -28,9 +29,14 @@ class CategProducts extends React.Component {
       <div>
         {productByCategory.map((product) => (
           <section key={ product.id }>
-            <p data-testid="product">{product.title}</p>
-            <img src={ product.thumbnail } alt={ product.title } />
-            <p>{product.price}</p>
+            <Link
+              to={ `/ProductPage/${product.id}` }
+              data-testid="product-detail-link"
+            >
+              <p data-testid="product">{product.title}</p>
+              <img src={ product.thumbnail } alt={ product.title } />
+              <p>{product.price}</p>
+            </Link>
           </section>
 
         ))}
