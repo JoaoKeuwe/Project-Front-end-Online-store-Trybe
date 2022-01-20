9501256785ff51ac66259636fc8cf4faf347fbef
Nicole Calderari <nicole_calderari@hotmail.com>
2022-01-19 21:13:46 -0300

    requisito 05 bugado

---
+++ src/components/CardProducts.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class CardProducts extends React.Component {
+    render() { 
+        
+        return (
+            <section>
+                <p> </p>
+                <img />
+                <p> </p>
+            </section>
+        );
+    }
+}
+ 
+export default CardProducts;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ProductList from './ProductList';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class Search extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      query: '',
+      apiProducts: [],
+    }
+  }
+
+handleSearchInput(typeEvent){
+  const input = typeEvent.target.value;
+  this.setState({
+    query: input,
+  })
+}
+
+handleSearchClick() {
+  const query = this.state
+  getProductsFromCategoryAndQuery(null, query).then((products) => {
+   this.setState({
+    apiProducts: products,
+   })
+  })
+}
+
   render() {
+    const { apiProducts } = this.state;
     return (
       <section>
         <form>
@@ -11,14 +38,32 @@ class Search extends React.Component {
               Digite algum termo de pesquisa ou escolha uma categoria.
             </p>
             <input
+              data-testid="query-input" 
               type="text"
               name="search"
               id="search"
-              placeholder="Digite algum termo de pesquisa ou escolha uma categoria."
+              onChange={this.handleSearchInput.bind(this)}
+              placeholder="Digite algum termo "
             />
           </label>
+          <button
+            type="button"
+            data-testid="query-button"
+            onClick={this.handleSearchClick.bind(this)}
+          >
+            Pesquisar
+          </button>
         </form>
         <ProductList />
+        <div>
+          {apiProducts.map((product) => (
+                     <section key={product.id}>
+                     <p data-testid="product">{product.title}</p>
+                     <img src={product.thumbnail} alt={product.title} />
+                     <p>{product.price}</p>
+                     </section>
+          ))}
+        </div>
       </section>
     );
   }
